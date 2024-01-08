cd5474ad75695e23131b90aadc489bc5bd1c49b8
lucio-cornejo <luciocornejo11@gmail.com>
2024-01-08 02:25:39 -0500

    begin creation of notes grid component

--- src/components/GridLayout.jsx
+++ src/components/GridLayout.jsx
@@ -1,6 +1,24 @@
-import WebmidiTest from './../connect-browser-to-DAW'
-const { midiController, midiChannel } = await WebmidiTest();
+import midiControls from './../connect-browser-to-DAW'
+const { midiController, midiChannel } = midiControls;
+
+const Grid = ( {
+  numRows = 8, numCols = 8, rightShift = 1, downShift = 4
+} ) => {
+  const rows = [...Array(numCols).keys()]
+    .map(cell => <p key={cell}>cell</p>);
+
+  return (
+    <>
+      <div className="notes-grid-layout">
+        {rows}
+      </div>
+    </>
+  )  
+
+}
 
 // midiChannel;
 //   .sendPitchBend(-0.5)
 //   .sendPitchBend(0.5, {time:'+200ms'})
+
+export default Grid
--- src/connect-browser-to-DAW.js
+++ src/connect-browser-to-DAW.js
@@ -1,37 +1,35 @@
 import { WebMidi } from "webmidi";
 const { VITE_MIDI_CONTROLLER_NAME, VITE_VIRTUAL_MIDI_NAME } = import.meta.env;
 
-export default async function WebmidiTest() {
-  const midiControls = await WebMidi
-    .enable()
-    .then(onEnabled)
-    .catch(err => alert(err));
+const midiControls = await WebMidi
+  .enable()
+  .then(onEnabled)
+  .catch(err => alert(err));
 
-  function onEnabled() {
-    console.log('MIDI enabled!');
+function onEnabled() {
+  console.log('MIDI enabled!');
 
-    WebMidi.inputs.forEach(input => console.log(input.name, input.id));
-    WebMidi.outputs.forEach(output => console.log(output.name, output.id));
-  
-    const midiController = WebMidi.getInputByName(VITE_MIDI_CONTROLLER_NAME);
-    const virtualMidiOutput = WebMidi.getOutputByName(VITE_VIRTUAL_MIDI_NAME);
+  WebMidi.inputs.forEach(input => console.log(input.name, input.id));
+  WebMidi.outputs.forEach(output => console.log(output.name, output.id));
 
-    const soundChannel = virtualMidiOutput.channels[1];
-    console.log(virtualMidiOutput.channels);
+  const midiController = WebMidi.getInputByName(VITE_MIDI_CONTROLLER_NAME);
+  const virtualMidiOutput = WebMidi.getOutputByName(VITE_VIRTUAL_MIDI_NAME);
 
-    midiController.addListener("noteon", e => {
-      soundChannel.playNote(e.note.identifier, {attack : e.velocity});
-    });
+  const soundChannel = virtualMidiOutput.channels[1];
+  console.log(virtualMidiOutput.channels);
 
-    midiController.addListener("noteoff", e => {
-      soundChannel.stopNote(e.note.identifier);
-    });
+  midiController.addListener("noteon", e => {
+    soundChannel.playNote(e.note.identifier, {attack : e.velocity});
+  });
 
-    return {
-      "midiController" : midiController, 
-      "midiChannel" : soundChannel,
-    }
+  midiController.addListener("noteoff", e => {
+    soundChannel.stopNote(e.note.identifier);
+  });
+
+  return {
+    "midiController" : midiController, 
+    "midiChannel" : soundChannel,
   }
-  
-  return midiControls;
 }
+
+export default midiControls;
